20d7a69adb92cbe7852de162f987afceaaf2a1a1
Ivan Dalmet <ivan@dalmet.fr>
2022-03-31 17:40:33 +0200

    fix: Add strict mode

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,14 +2,12 @@ import { Button } from "ui";
 import { trpc } from "../utils/trpc";
 
 export default function Web() {
-
-  const { data } = trpc.useQuery(['hello']);
+  const { data } = trpc.useQuery(["hello", { name: "world" }]);
 
   return (
     <div>
-      <h1>{data}</h1> 
+      <h1>{data}</h1>
       <Button />
     </div>
   );
 }
- 
